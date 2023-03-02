f1f6a4d17a9b3a565f7156189fd366eac0438e01
Julien Portalier <julien@portalier.com>
2023-03-02 12:49:27 +0100

    Lock Go dependencies + multistage Docker build (#1)
    * Fix: upgrade/lock docker dependencies + multi-stage dockerfile
    
    * Fix: Dockerfile
    
    * Fix: lock the Golang version (:shame:)
    
    Co-authored-by: Matías García Isaía <mgarcia@manas.tech>
    
    ---------
    
    Co-authored-by: Matías García Isaía <mgarcia@manas.tech>

--- monitor.go
+++ monitor.go
@@ -13,8 +13,13 @@ func main() {
 		log.Fatal(err)
 	}
 
-	listener := make(chan *docker.APIEvents)
-	err = cli.AddEventListener(listener)
+	listener := make(chan *docker.APIEvents, 10)
+	filters := map[string][]string{
+		"type":  {"container"},
+		"event": {"create"},
+	}
+	opts := docker.EventsOptions{Filters: filters}
+	err = cli.AddEventListenerWithOptions(opts, listener)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -25,33 +30,30 @@ func main() {
 	for {
 		event := <-listener
 
-		if event.Type == "container" && event.Action == "create" {
-			containerName := event.Actor.Attributes["name"]
-			log.Printf("New container (%s) created\n", containerName)
+		containerName := event.Actor.Attributes["name"]
+		log.Printf("New container (%s) created\n", containerName)
 
-			project, hasProject := event.Actor.Attributes["com.docker.compose.project"]
-			service, hasService := event.Actor.Attributes["com.docker.compose.service"]
-			oneoff := event.Actor.Attributes["com.docker.compose.oneoff"]
+		project, hasProject := event.Actor.Attributes["com.docker.compose.project"]
+		service, hasService := event.Actor.Attributes["com.docker.compose.service"]
+		oneoff := event.Actor.Attributes["com.docker.compose.oneoff"]
 
-			if hasProject && hasService {
-				config := docker.NetworkConnectionOptions{Container: event.Actor.ID}
+		if hasProject && hasService {
+			config := docker.NetworkConnectionOptions{Container: event.Actor.ID}
 
-				if oneoff == "False" {
-					alias := service + "." + project + "." + domain
-					log.Printf("Attaching %s to the shared network with alias %s\n", containerName, alias)
-					config.EndpointConfig = &docker.EndpointConfig{
-						Aliases: []string{alias},
-					}
-				} else {
-					log.Printf("Attaching %s to the shared network\n", event.Actor.ID)
+			if oneoff == "False" {
+				alias := service + "." + project + "." + domain
+				log.Printf("Attaching %s to the shared network with alias %s\n", containerName, alias)
+				config.EndpointConfig = &docker.EndpointConfig{
+					Aliases: []string{alias},
 				}
+			} else {
+				log.Printf("Attaching %s to the shared network\n", event.Actor.ID)
+			}
 
-				err := cli.ConnectNetwork("shared", config)
-				if err != nil {
-					log.Fatal(err)
-				}
+			err := cli.ConnectNetwork("shared", config)
+			if err != nil {
+				log.Fatal(err)
 			}
 		}
 	}
-
 }
